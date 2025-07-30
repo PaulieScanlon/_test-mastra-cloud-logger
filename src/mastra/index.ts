8ccd13dbaccc6bb8db3c1173968c2a4d33ab6507
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-30 12:11:23 -0500

    test: add vercel deployer

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,16 +1,18 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
+// import { LibSQLStore } from "@mastra/libsql";
+import { VercelDeployer } from "@mastra/deployer-vercel";
 import { testAgent } from "./agents/test-agent";
 import { testWorkflow } from "./workflows/test-workflow";
 
 export const mastra = new Mastra({
   workflows: { testWorkflow },
   agents: { testAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:"
-  }),
+  // storage: new LibSQLStore({
+  //   // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+  //   url: ":memory:"
+  // }),
+  deployer: new VercelDeployer(),
   logger: new PinoLogger({
     name: "Mastra",
     level: "info"
